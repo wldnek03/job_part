00d9348b7139063dba30b211af9f6a844917ab1c
wldnek03 <135032357+wldnek03@users.noreply.github.com>
2024-12-15 19:12:11 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
